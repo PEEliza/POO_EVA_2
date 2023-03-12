9e6e0b44e5eff8d3c04a3465eddfa0f9f71ed6a5
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-03-12 14:37:37 -0600

    Add files via upload

---
+++ EVA2_5_ TIPOS_HERENCIA/src/eva2_5_/tipos_herencia/EVA2_5_TIPOS_HERENCIA.java
@@ -0,0 +1,29 @@
+
+package eva2_5_.tipos_herencia;
+
+
+public class EVA2_5_TIPOS_HERENCIA {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
+// 
+
+class SuperClase{
+    
+    
+}
+
+class OtraSuperClase{// NO SE PUEDE HEREDAR DE DOS O
+                     // MÁS SUPERCLASES
+    
+}
+                     // extends solo funciona para una
+                     // unica clase
+
+class BaseClase extends SuperClase{
+
+}
