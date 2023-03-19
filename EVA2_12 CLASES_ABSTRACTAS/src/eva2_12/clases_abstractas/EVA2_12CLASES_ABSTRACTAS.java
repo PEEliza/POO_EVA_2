0ee299eaad885f58f8b63ca32e90322fa0f045e4
Poleth Escarcega Lopez <124701872+PEEliza@users.noreply.github.com>
2023-03-19 16:54:13 -0600

    Add files via upload

---
+++ EVA2_12 CLASES_ABSTRACTAS/src/eva2_12/clases_abstractas/EVA2_12CLASES_ABSTRACTAS.java
@@ -0,0 +1,23 @@
+
+package eva2_12.clases_abstractas;
+// AUTOR: POLETH E. ESCARCEGA LOPEZ
+
+public class EVA2_12CLASES_ABSTRACTAS {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Prueba prueba = new Prueba();
+        PruebaAbstract pAbs = new PruebaAbstract(); //NO SE PUEDEN CREAR OBJETOS (INSTANCIAS)
+        // DE CLASES ABSTRACTAS
+    }
+    
+}
+
+class Prueba{
+    
+}
+
+abstract class PruebaAbstract{
+    
+}
